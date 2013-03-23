ecd7613b259193d416ee7bdf8659c77f188758c9
Michal Kuklis <michal.kuklis@gmail.com>
2013-03-23 18:03:40 -0400

    more work on styluys

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,18 @@
 module.exports = function (grunt) {
 
-  grunt.initConfig({
+  var swatches = grunt.file.readJSON('swatches.json');
 
+  grunt.initConfig({
+    stylus: grunt.util._(swatches).reduce(function (memo, value, key) {
+      var swatch = { options: {}, files: {} };
+      swatch.options.define = value;
+      swatch.options.define.swatch = key;
+      swatch.files['themes/css/swatch-' + key + '.css'] =  ['./themes/stylus/swatch.styl']
+      memo[key] = swatch;
+      return memo;
+    }, {})
   });
 
-  grunt.loadTasks("tasks");
   grunt.loadNpmTasks('grunt-contrib-stylus');
-
+  grunt.registerTask('default', ['stylus']);
 };
