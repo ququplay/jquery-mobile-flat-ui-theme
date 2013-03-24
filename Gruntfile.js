a368ac82c0f74825269f2415b08ea859cd8fe822
Michal Kuklis <michal.kuklis@gmail.com>
2013-03-23 23:59:16 -0400

    more changes to swatches

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,13 @@
 module.exports = function (grunt) {
 
-  var swatches = grunt.file.readJSON('swatches.json');
-
   grunt.initConfig({
-    stylus: grunt.util._(swatches).reduce(function (memo, value, key) {
-      var swatch = { options: {}, files: {} };
-      swatch.options.define = value;
-      swatch.options.define.swatch = key;
-      swatch.files['themes/css/swatch-' + key + '.css'] =  ['./themes/stylus/swatch.styl']
-      memo[key] = swatch;
-      return memo;
-    }, {})
+    stylus: {
+      compile: {
+        files: {
+          'themes/css/test.css': ['themes/stylus/swatches/*.styl']
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-stylus');
